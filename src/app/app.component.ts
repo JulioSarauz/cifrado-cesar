4861718c93c1a18edb6e038ca3cae1626ad848f6
Julio Saráuz <juliosarauz1i@gmail.com>
2020-12-02 01:25:55 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,75 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cesar';
+  saltos:number;
+  msm1:string;    
+  msm2:string;
+  letras:string = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+  letras2:string;
+  alfa:Array<data> = [];
+  alfa2:Array<data> = [];
+  l:data;
+  
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    
+      for (let i=0; i < this.letras.length; i++){
+        this.l = { id:i, letra:this.letras.charAt(i)};
+        this.alfa.push(this.l);
+      }
+  } 
+
+
+  NumeroSaltos(){
+    this.alfa2 = [];
+    this.letras2 = "";
+    for (let i=0 + this.saltos; i < this.letras.length; i++){
+      this.l = { id:i, letra:this.letras.charAt(i)};
+      this.alfa2.push(this.l);
+    }
+    for(let j = 0; j < this.saltos; j++){
+      this.l = { id:j, letra:this.letras.charAt(j)};
+      this.alfa2.push(this.l);
+    }
+    for(let w of this.alfa2){
+      this.letras2 = this.letras2 + w.letra;
+    }
+    
+  }
+
+  Encriptar(){
+    let texto = "";
+    let pos = 0;
+    console.log(this.letras2);
+    for(let i = 0; i < this.msm1.length; i++){
+      pos = this.letras2.indexOf(this.msm1.charAt(i).toUpperCase());
+      texto = texto + this.alfa[pos].letra;       
+    }
+    this.msm2 = texto;
+  }
+
+
+
+  Desencriptar(){
+    let texto = "";
+    let pos = 0;
+    for(let i = 0; i < this.msm2.length; i++){
+      pos = this.letras2.indexOf(this.msm2.charAt(i).toUpperCase());
+      texto = texto + this.alfa[pos].letra;       
+    }
+    this.msm1 = texto;
+  }
+
+}
+
+export class ModelComponent {
+  val: number;
+} 
+
+interface data{
+  id:number;
+  letra:string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import { FormsModule } from '@angular/forms';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {InputTextModule} from 'primeng/inputtext';
+import {RippleModule} from 'primeng/ripple';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    InputTextareaModule,
+    FormsModule,
+    MatGridListModule,
+    MatToolbarModule,
+    InputTextModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
